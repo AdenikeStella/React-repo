c83048b8b040012aae42945480e00b2cb8c00943
Your Name <your.email@example.com>
2025-03-29 15:26:30 +0100

    added a new header h6

--- src/Component/Heading.js
+++ src/Component/Heading.js
@@ -1,8 +1,12 @@
 function Heading() { 
     return ( 
+      <div>
       <h1>I want to see if I actually know what I'm doing in Git.
         <br>MO-CHARM-APP</br>
       </h1>
+
+      <h6>So I tried something else and I guess I understand git so far!</h6>
+      </div>
     ) 
   };
 

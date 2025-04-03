22fe0e643d0b2e2ebd4a36b6af43cfee30fe2413
Your Name <your.email@example.com>
2025-04-03 13:42:18 +0100

    let's try this

--- src/Component/Promo.js
+++ src/Component/Promo.js
@@ -1,10 +1,9 @@
-const data = {
-    heading: "99% discount available on all first time purchases!",
-    callToAction: "All items must go!"
-}
-
-
 function Promo(props) {
+    const data = {
+        heading: "99% discount available on all first time purchases!",
+        callToAction: "All items must go!"
+    }
+    
     return (
         <div className="promo-section">
             <h1>{props.heading}</h1>
